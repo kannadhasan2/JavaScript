b61a106f86062b680d4bc96c752c7d9af188362a
kannadhasan <kannadhasansivakumar2005@gmail.com>
2026-01-21 20:43:13 +0530

    Day -1 of JS Practice

---
+++ loops.js
@@ -0,0 +1,30 @@
+// For loop 
+for(let i = 0;i<10;i++){
+    console.log(i) 
+}
+
+// While loop 
+let x = 1 
+while (x < 10){
+    console.log(x)
+    x++;
+}
+
+// Do-While loop 
+x = 0 
+do{
+    console.log(x)
+    x++
+}while (x < 10); 
+
+// For of loop 
+let arr = [20,3,13,44]
+for (let i of arr){
+    console.log(i)
+}
+
+// for in loop 
+let obj = {name:"kanna",age:21}
+for(let key in obj){
+    console.log(key, obj[key])
+}
