cbdf6a5957e438a7f6ba70648ff7d55a2bbfec9a
kannadhasan <kannadhasansivakumar2005@gmail.com>
2026-01-21 21:03:55 +0530

    Day -1 of JS Practice

---
+++ null.js
@@ -0,0 +1,7 @@
+// Null 
+
+let x = null 
+console.log(x) // null
+console.log(typeof(x)) // Object
+
+// Null is used in the case where we intensionally want a varibale but don't need a value on it.
